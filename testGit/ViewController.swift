27b26cb1f1377e28be0b0947c23940b3fef9878e
sdmken <116410449+sdmken@users.noreply.github.com>
2024-08-21 20:39:51 +0900

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by 堤健二 on 2024/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
